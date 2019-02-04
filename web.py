456fc381c9d18f0d711d88cc14ce82ec932041fd
the-crazy <thecrazy.git@gmail.com>
2019-02-04 16:21:38 -0200

    Edit PORT number

--- web.py
+++ web.py
@@ -22,5 +22,5 @@ def resultados():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(host='127.0.0.1', port=port)
